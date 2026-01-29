2ad7a328dc9b20d817990c8aa0577380a31f7351
GreenEdgeDev <greenedgedev@gmail.com>
2026-01-29 16:56:18 +0100

    allocation of vertex buffer object memory

--- Learning_OpenGL.cpp
+++ Learning_OpenGL.cpp
@@ -11,7 +11,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Generate window and context
+	// Generate window and OpenGL context
 	GLFWwindow* window = glfwCreateWindow(800, 600, "MyWindowTitle", NULL, NULL);
 	if (window == NULL) {
 		std::cout<<"Failed to create window."<<std::endl;
@@ -28,21 +28,56 @@ int main() {
 	}
 	glViewport(0, 0, 800, 600);
 	
-	// function declaration start
+	// function declarations start
+
 	void frame_buffer_size_callback(GLFWwindow* window, int width, int height);
 	void processInput(GLFWwindow * window);
-	// function declaration end
 
+	// function declarations end
 
 	// set window resize callback
 	glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
 
+	// Data Start
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f
+	};
+
+	unsigned int VBO;
+	// generate a buffer object name in OpenGL and assign one unique integer to the VBO variable
+	glGenBuffers(1, &VBO);
+
+	// use the buffer object as a vertex buffer object. Anytime we target GL_ARRAY_BUFFER,
+	// we would refer to the recently bound buffer which in this case would be VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	// allocate memory on the GPU for the buffer object currently bound to GL_ARRAY_BUFFER which in this case is VBO
+	// then store the vertices with a hint on how it would be accessed in this case GL_STATIC_DRAW
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Data End
+
 	// Render Loop start
+
 	while (!(glfwWindowShouldClose(window))) {
 		processInput(window);
+
+		// rendering start
+
+		// set the state of the display color
+		glClearColor(0.25f, 0.25f, 1.0f, 1.0f);
+		// replace the current state with the previously declared display state
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// rendering end
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-	} 
+	}
+	
 	// Render Loop end
 
 	glfwTerminate();
